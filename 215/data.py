101910588fc1aca253b7f117b6d8f7f9188f02e1
Bulbazavriq <viktor.kluchkovsky@gmail.com>
2020-06-07 13:52:43 +0500

    измнения

--- 215/data.py
+++ 215/data.py
@@ -1,6 +1,6 @@
 data = [
-        {'country': '', 'capital': '', 'square': 333, 'qty': 333, 'continent': 'Europe|Asia'},
-        {'country': '', 'capital': '', 'square': 333, 'qty': 333, 'continent': 'Europe|Asia'},
-        {'country': '', 'capital': '', 'square': 333, 'qty': 333, 'continent': 'Europe|Asia'},
-        {'country': '', 'capital': '', 'square': 333, 'qty': 333, 'continent': 'Europe|Asia'},
+        {'country': 'Russia', 'capital': 'Moscow', 'square': 333, 'qty': 333, 'continent': 'Europe'},
+        {'country': 'Francia', 'capital': 'Parizh', 'square': 333, 'qty': 333, 'continent': 'Europe'},
+        {'country': 'Germania', 'capital': 'Berlin', 'square': 333, 'qty': 333, 'continent': 'Asia'},
+        {'country': 'Italia', 'capital': 'Rim', 'square': 333, 'qty': 333, 'continent': 'Asia'},
 ]
