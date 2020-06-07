83e6392ae800b1a378d174f6e64798e3365f68d5
Sergey Kluchkovsky <kaineer@gmail.com>
2020-06-07 20:26:09 +0500

    Правит данные в 215

--- 215/data.py
+++ 215/data.py
@@ -1,6 +1,7 @@
 data = [
         {'country': 'Russia', 'capital': 'Moscow', 'square': 333, 'qty': 333, 'continent': 'Europe'},
         {'country': 'Francia', 'capital': 'Parizh', 'square': 333, 'qty': 333, 'continent': 'Europe'},
-        {'country': 'Germania', 'capital': 'Berlin', 'square': 333, 'qty': 333, 'continent': 'Asia'},
-        {'country': 'Italia', 'capital': 'Rim', 'square': 333, 'qty': 333, 'continent': 'Asia'},
+        {'country': 'Germania', 'capital': 'Berlin', 'square': 333, 'qty': 333, 'continent': 'Europe'},
+        {'country': 'Italia', 'capital': 'Rim', 'square': 333, 'qty': 333, 'continent': 'Europe'},
+        {'country': 'China', 'capital': 'Beijin', 'square': 500, 'qty': 1000, 'continent': 'Asia'}
 ]
